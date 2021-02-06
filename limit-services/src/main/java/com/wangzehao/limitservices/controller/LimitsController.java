17d27aa07c551fdf2236c4238753aef659236f85
wzh <wangzehao1998@126.com>
2021-02-06 10:46:36 +0800

    get configuration from config repo
    limit-service->>config-server->>config repo

--- limit-services/src/main/java/com/wangzehao/limitservices/controller/LimitsController.java
+++ limit-services/src/main/java/com/wangzehao/limitservices/controller/LimitsController.java
@@ -1,14 +1,19 @@
 package com.wangzehao.limitservices.controller;
 
 import com.wangzehao.limitservices.bean.Limits;
+import com.wangzehao.limitservices.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
 
+    @Autowired
+    private Configuration configuration;
+
     @GetMapping("/limits")
     public Limits retrieveLimits() {
-        return new Limits(1, 1000);
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
     }
 }
