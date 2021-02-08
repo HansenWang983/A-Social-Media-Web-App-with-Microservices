c83c0902107e6d2ed65a9b96fdb84719a19e6a98
wzh <wangzehao1998@126.com>
2021-02-08 09:36:14 +0800

    Creating a GET service for currency conversion

---
+++ currency-conversion-service/src/main/java/com/wangzehao/currencyconversionservice/CurrencyConversionController.java
@@ -0,0 +1,26 @@
+package com.wangzehao.currencyconversionservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyConversionController {
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversion(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity){
+        String env = environment.getProperty("local.server.port");
+        return new CurrencyConversion(10001L,
+                from, to, quantity,
+                BigDecimal.ONE,
+                BigDecimal.ONE,
+                env);
+    }
+}
